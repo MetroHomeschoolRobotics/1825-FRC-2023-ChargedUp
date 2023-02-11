135f584ea379d1e1e599f72ad36f3d2175570958
xplodingpsyduck <samhutchkc@gmail.com>
2023-02-11 11:51:51 -0600

    vision updates

---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.*;
+public class Vision extends SubsystemBase{
+    public Vision() {}
+    public PhotonCamera aprilTagCam = new PhotonCamera("aprilTag1");
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putBoolean("aprilTagHasTargets", aprilTagCam.getLatestResult().hasTargets());
+        
+    }
+}
