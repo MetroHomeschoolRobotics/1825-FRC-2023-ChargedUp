d65a1f586781be8a3ee37d77617d413f91057782
Leviathan1000 <jrweg1234@hotmail.com>
2023-02-25 11:59:36 -0600

    2/25/23

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
     public final static double ramseteZeta = 0.7;
 
 
-    public static Trajectory CurveThenBalance = TrajectoryHelper.generateFromPathPlanner("Curvethenbalance");
-    public static Trajectory Straight5meters = TrajectoryHelper.generateFromPathPlanner("Straight5meters");
+    public static Trajectory CurveThenBalance = TrajectoryHelper.generateFromPathPlanner("CurvePath");
+    public static Trajectory Straight5meters = TrajectoryHelper.generateFromPathPlanner("New New Path");
     public static Trajectory Straight6meters = TrajectoryHelper.generateFromPathPlanner("Straight6meters");
 }
