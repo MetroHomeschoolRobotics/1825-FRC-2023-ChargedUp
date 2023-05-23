19e9069013d580758ce2d73bc934f6e387729259
David Morgan <dmorgan@aechelon.com>
2023-05-22 21:49:43 -0500

    Dual Motor Telescope

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -16,6 +16,7 @@ import frc.robot.Constants;
 public class Arm extends SubsystemBase {
   CANSparkMax angleMotor = new CANSparkMax(5, MotorType.kBrushless);
   CANSparkMax telescopingMotor = new CANSparkMax(6, MotorType.kBrushless);
+  CANSparkMax telescopingMotor2 = new CANSparkMax(7, MotorType.kBrushless);
   DutyCycleEncoder rotationEncoder = new DutyCycleEncoder(0);
   private DigitalInput BeamBreakSensor;
   /** Creates a new Arm. */
@@ -25,18 +26,26 @@ public class Arm extends SubsystemBase {
 
     angleMotor.setSmartCurrentLimit(60);
     telescopingMotor.setSmartCurrentLimit(35);
+    telescopingMotor2.setSmartCurrentLimit(35);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    // automatically zero the tele encoder when beam break is triggered
+    if(!BeamBreakSensor.get())
+    {
+      resetTeleEncoders();
+    }
+
     SmartDashboard.putNumber("Arm Motor Position", angleMotor.getEncoder().getPosition());
     SmartDashboard.putNumber("Shaft Encoder Position", rotationEncoder.getAbsolutePosition());
     SmartDashboard.putNumber("ExtensionEncoderValue", telescopingMotor.getEncoder().getPosition());
     SmartDashboard.putNumber("Telescoping distance!!!", getTeleEncoderDistance());
     SmartDashboard.putNumber("Absolute Angle", getAbsoluteAngle());
     SmartDashboard.putNumber("Motor Input", setArmStability(getAbsoluteAngle(), getTeleEncoderDistance()));
-    SmartDashboard.putBoolean("Intake Sensor", !BeamBreakSensor.get());
+    SmartDashboard.putBoolean("Retract Sensor", !BeamBreakSensor.get());
   }
 //The motor encoder for the rotating arm
   public void resetAngleEncoders(){
@@ -68,7 +77,9 @@ public class Arm extends SubsystemBase {
     return rotationEncoder.getAbsolutePosition();
   }
   public double getAbsoluteAngle(){
-    return ((rotationEncoder.getAbsolutePosition()-.63)*(360/1)+16); // this encoder used rotations as a position mesurement so we move the degrees to the top and convert it to degrees with this method
+    final double discontinuityOffset = 1.25;  // move the encoder's discontinuity where the arm can't reach
+    final double calibrationOffset = -.63+.129;  // calibrate so zero degrees is vertical
+    return (((rotationEncoder.getAbsolutePosition()+discontinuityOffset) % 1.0+calibrationOffset)*(360.0)); // this encoder used rotations as a position mesurement so we move the degrees to the top and convert it to degrees with this method
   }
   
 //The motor encoder for the telescoping arm
@@ -89,6 +100,7 @@ public class Arm extends SubsystemBase {
   }
   public void moveTeleMotor(double speed){
     telescopingMotor.set(speed); //Changed this motor from anglemotor to telescoping motor - Joseph
+    telescopingMotor2.set(speed);
   }
 
   public boolean getBeamBreakSensor() {//reads true if triggered for retracting the arm.
@@ -98,11 +110,19 @@ public class Arm extends SubsystemBase {
 
 public double setArmStability(double angle, double radius){ //this equation helps the arm to fight gravity which is affected by the angle and arm extension length
     
-  double maxForce = 0.000303*radius + 0.0156;
+  //Maximum radius = 111.14
+  //Desired result at full extension. horizontal is 0.55
+  //Desired result at full retraction, horizontal is 0.0859
+  //double maxForce = 0.000303*radius + 0.0156;
+  final double retractedForce = 0.03693;
+  final double extendedForce = 0.234375;
+  final double extendedRadius = 110.0;
+  final double maxForce = (extendedForce - retractedForce)*radius/extendedRadius + retractedForce;
   
   double force = Math.sin(angle*(Math.PI/180))*maxForce;  // to get the input needed, we take the sine of the angle from the top and increase the amplitude depending on how extended the arm is times some constant
-  /* Moved the maxforce to outside the sin function.
-  Joseph B*/ 
+ 
+  SmartDashboard.putNumber("Arm Stability Force", force);
+
   return force;
 }
 
