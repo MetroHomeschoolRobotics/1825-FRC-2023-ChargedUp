49ebba9c2a8e239ab1182745f895fa563c5a4d76
MetroHomeschoolRobotics <code@metrohomeschoolrobotics.org>
2023-11-20 21:30:25 -0600

    Worked on improving the turn to apriltag command

--- src/main/java/frc/robot/commands/DriveToApril.java
+++ src/main/java/frc/robot/commands/DriveToApril.java
@@ -15,12 +15,12 @@ public class DriveToApril extends CommandBase {
   private Drivetrain drivetrain;
   private Limelight limelight;
 
-  private static double kp = 0.6;
+  private static double kp = 0.0275;
   private static double ki = 0;
   private static double kd = 0;
   private PIDController pidR = new PIDController(Constants.kpDriveVel, 0, 0);
   private PIDController pidL = new PIDController(Constants.kpDriveVel, 0, 0);
-  private PIDController turnPID = new PIDController(0.005, ki, kd);
+  private PIDController turnPID = new PIDController(kp, ki, kd);
 
   private static double distance;
   private static double turnSpeed;
@@ -39,10 +39,14 @@ public class DriveToApril extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    distance = limelight.getNodeAprilDistance()-1;
-
-  }
-
+    if(limelight.hasTargests()){
+    //distance = limelight.getNodeAprilDistance()-1; //TODO why's this one here?
+    distance = 1;
+    }
+  else{
+    distance = 1;  // You have to have a default value otherwise the code crashes
+ }
+}
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
@@ -51,6 +55,7 @@ public class DriveToApril extends CommandBase {
     if(limelight.hasTargests()){
       turnSpeed = turnPID.calculate(limelight.getTargetYaw(), 2.7);
       System.out.println(limelight.getTargetYaw());
+      drivetrain.getSignal();
     }else{
       turnSpeed = 0;
       drivetrain.getSignal();
